c8be96c0e7a8220574446b813fc3ce4ba4253abd
carlos <carlos_wow55@hotmail.com>
2022-04-04 03:51:13 -0500

    se creo el metodo de crear

--- proyect/Api/Models/model.py
+++ proyect/Api/Models/model.py
@@ -6,17 +6,24 @@ from proyect import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text, nullable=False, unique=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
     full_name = db.Column(db.Text, nullable=False)
-    vehiculo = db.relationship('Behiculo', backref='user', lazy=True)
+    vehiculo = db.relationship('Vehiculo', backref='user', lazy=True)
 
 class Vehiculo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    plates = db.Column(db.Text, nullable=False, unique=True)
-    last_position = db.Column(db.Text, nullable=False)
+    plates = db.Column(db.String(20), nullable=False)
+    lat = db.Column(db.String(20), nullable=False)
+    lon = db.Column(db.String(20), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
         nullable=False)
+
+    def __init__(self, plates, lat, lon, user_id):
+        self.plates = plates
+        self.lat = lat
+        self.lon = lon
+        self.user_id = user_id
 
 
 db.create_all()
--- proyect/Api/routes/RouteMain.py
+++ proyect/Api/routes/RouteMain.py
@@ -6,8 +6,28 @@ sys.path.append(".")
 
 routes = Blueprint('route',__name__)
 
+from proyect import db
+from proyect.Api.Models.model import Vehiculo
+
+
 class route():
 
     @routes.route("/", methods=["GET"])
     def login():
         return 'hola mundo'
+
+    @routes.route("/create", methods=["POST","GET"])
+    def create():
+
+        plates =  request.values.get("plates", type=str, default=None)
+        lat =  request.values.get("lat", type=str, default=None)
+        lon =  request.values.get("lon", type=str, default=None)
+
+        user_id = 1
+
+        new_vehiculo = Vehiculo(plates, lat, lon, user_id)
+
+        db.session.add(new_vehiculo)
+        db.session.commit()
+
+        return jsonify({"message": "vehiculo agregado"}) 
